aae223615154554840aff05d1bcbe62bf023e41f
sr sohan <sr.sohan088@gmail.com>
2024-11-15 18:10:21 +0600

    add email

--- utils/mailer.js
+++ utils/mailer.js
@@ -4,8 +4,8 @@ const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    user: process.env.USER_EMAIL,
-    pass: process.env.EMAIL_KEY,
+    user: "clemsongclab@gmail.com",
+    pass: "viif wdtx kslm ztbb",
   },
 });
 
